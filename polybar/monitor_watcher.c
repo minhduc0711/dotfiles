f85f5c7e8d9d72b039135af3406f2a67d03add64
minhduc0711 <phamminhduc0711@gmail.com>
2020-06-02 22:52:00 +0700

    config polybar for monitor hotplugging

---
+++ polybar/monitor_watcher.c
@@ -0,0 +1,50 @@
+/*
+ * Compile with -lxcb -lxcb-randr
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <xcb/xcb.h>
+#include <xcb/randr.h>
+
+int main() {
+  xcb_connection_t* conn;
+  xcb_screen_t* screen;
+  xcb_window_t window;
+  xcb_generic_event_t* evt;
+  xcb_randr_screen_change_notify_event_t* randr_evt;
+  time_t t0 = 0;
+  time_t t1;
+
+  conn = xcb_connect(NULL, NULL);
+
+  screen = xcb_setup_roots_iterator(xcb_get_setup(conn)).data;
+  window = screen->root;
+  xcb_randr_select_input(conn, window, XCB_RANDR_NOTIFY_MASK_SCREEN_CHANGE);
+  xcb_flush(conn);
+
+  while ((evt = xcb_wait_for_event(conn)) != NULL) {
+    if (evt->response_type & XCB_RANDR_NOTIFY_MASK_SCREEN_CHANGE) {
+      randr_evt = (xcb_randr_screen_change_notify_event_t*) evt;
+      t1 = time(NULL);
+      if (t1 - t0 > 2) {
+        int pid = fork();
+        if (pid == 0) {
+          printf("you did it\n");
+          char *name[] = {
+            "/bin/bash",
+            "-c",
+            "~/.config/polybar/launch.sh",
+            NULL
+          };
+          execvp(name[0], name);
+        }
+      }
+      t0 = t1;
+    }
+    free(evt);
+  }
+  xcb_disconnect(conn);
+}
