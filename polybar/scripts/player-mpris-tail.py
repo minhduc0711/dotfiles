a1ba7e9f0fc8a66b37c92a5e38ab24549b9b259f
minhduc0711 <phamminhduc0711@gmail.com>
2020-08-02 21:47:45 +0700

    fix spotify polybar aaaaaaaaaaaaaaaa

--- polybar/scripts/player-mpris-tail.py
+++ polybar/scripts/player-mpris-tail.py
@@ -514,8 +514,11 @@ else:
         current_player.previous()
     elif args.command == 'next' and current_player:
         current_player.next()
-    elif args.command == 'status' and current_player:
-        current_player.printStatus()
+    elif args.command == 'status':
+        if current_player:
+            current_player.printStatus()
+        else:
+            _printFlush("No music playing")
     elif args.command == 'list':
         print("\n".join(sorted([
             "{} : {}".format(player.bus_name.split('.')[3], player.status)
