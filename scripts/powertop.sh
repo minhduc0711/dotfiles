d03dfa113fa6d23955e3e764c207407924ae86d1
minhduc0711 <phamminhduc0711@gmail.com>
2021-04-03 19:28:08 +0700

    Update power saving settings
    TLP and powertop configs adapted from https://wiki.archlinux.org/index.php/Acer_Aspire_5_A515-51G-5072

--- scripts/powertop.sh
+++ scripts/powertop.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 powertop --auto-tune
+
 HIDDEVICES=$(ls /sys/bus/usb/drivers/usbhid | grep -oE '^[0-9]+-[0-9\.]+' | sort -u)
 for i in $HIDDEVICES; do
   echo -n "Enabling " | cat - /sys/bus/usb/devices/$i/product
-  echo 'on' > /sys/bus/usb/devices/$i/power/control
+  echo "on" > /sys/bus/usb/devices/$i/power/control
 done
+
+echo "Disabling Runtime PM for PCI Device Qualcomm Atheros QCA9377 802.11ac Wireless Network Adapter"
+echo "on" > /sys/bus/pci/devices/0000:03:00.0/power/control
