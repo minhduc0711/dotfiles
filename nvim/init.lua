8e4b24ba45e4ea90ab8eb0916de17c36aa52a29c
minhduc0711 <phamminhduc0711@gmail.com>
2021-08-26 12:02:02 +0700

    Remove annoying LSP error in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -258,16 +258,21 @@ cmd [[inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"]]
 map('i', '<C-t>', 'compe#confirm("<C-t>")', {expr = true})
 
 -- LSPinstall
-require'lspinstall'.setup() -- important
-local servers = require'lspinstall'.installed_servers()
-for _, server in pairs(servers) do
-  require'lspconfig'[server].setup{}
-end
 local function setup_servers()
   require'lspinstall'.setup()
   local servers = require'lspinstall'.installed_servers()
   for _, server in pairs(servers) do
-    require'lspconfig'[server].setup{}
+    local config = {}
+    if server == "lua" then
+      config.settings = {
+        Lua = {
+          diagnostics = {
+            globals = {'vim'}
+          }
+        }
+      }
+    end
+    require'lspconfig'[server].setup(config)
   end
 end
 setup_servers()
@@ -277,6 +282,7 @@ require'lspinstall'.post_install_hook = function ()
   cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
 end
 
+-- Custom vim commands for jdtls
 require'jdtls.setup'.add_commands()
 
 -- Telescope
