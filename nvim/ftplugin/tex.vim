d4955fde2e5ded55d32467ff88340b31a797abe4
minhduc0711 <phamminhduc0711@gmail.com>
2022-03-31 13:25:13 +0200

    Big commit after a while

--- nvim/ftplugin/tex.vim
+++ nvim/ftplugin/tex.vim
@@ -1,1 +1,7 @@
 runtime macros/ibus.vim
+
+setlocal autoindent
+let g:tex_flavor = "latex"
+
+let g:tex_indent_brace = 0
+let g:tex_indent_items = 0
--- nvim/init.lua
+++ nvim/init.lua
@@ -27,19 +27,37 @@ map('n', 'Q', '<NOP>')
 map('n', 'H', 'gT')
 map('n', 'I', 'gt')
 
--- LSP commands
-map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {silent=true})
-map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', {silent=true})
-map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', {silent=true})
-map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', {silent=true})
-map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {silent=true})
-map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', {silent=true})
-map('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', {silent=true})
-map('n', '<leader>l', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', {silent=true})
-map('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', {silent=true})
-map('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', {silent=true})
--- Java specific
-map('n', '<leader>ca', '<cmd>lua require"jdtls".code_action()<CR>', {silent=true})
+-- LSP
+local on_attach = function(_, bufnr)
+  local opts = { noremap = true, silent = true }
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua require"telescope.builtin".lsp_definitions{}<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua require"telescope.builtin".lsp_references{}<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>l', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  -- Java specific
+  -- map('n', '<leader>ca', '<cmd>lua require"jdtls".code_action()<CR>', {silent=true})
+end
+
+-- Searching
+-- fzf is still better for fuzzy search
+map('n', '<C-p>', ':Files<CR>', {silent = true})
+map('n', '<C-f>', ':Rg<CR>' , {silent = true})
+-- map('n', '<C-p>', ':Telescope find_files<CR>', {silent = true})
+-- map('n', '<C-f>', ':Telescope live_grep<CR>' , {silent = true})
+map('n', '<C-e>', ':Telescope lsp_document_diagnostics<CR>' , {silent = true})
+
+-- Seamless navigation between tmux & vim
+g.tmux_navigator_no_mappings = 1
+map('n', '<M-h>', ':TmuxNavigateLeft<cr>', {silent = true})
+map('n', '<M-n>', ':TmuxNavigateDown<cr>', {silent = true})
+map('n', '<M-e>', ':TmuxNavigateUp<cr>', {silent = true})
+map('n', '<M-i>', ':TmuxNavigateRight<cr>', {silent = true})
 
 ---------- GENERAL OPTIONS ----------
 
@@ -112,26 +130,38 @@ packer.startup(function(use)
   use {'neovim/nvim-lspconfig'}
   -- Eclipse Java LSP
   use {'mfussenegger/nvim-jdtls'}
-  -- Java w/ Eclipse
-  use {'starcraftman/vim-eclim'}
 
   -- Better LSP for Scala
   -- TODO: enable this when you start learning Scala again
-  -- use {'scalameta/nvim-metals'}
-  -- Easily install LSPs
-  use {'kabouzeid/nvim-lspinstall'}
+  use {
+    'scalameta/nvim-metals',
+    requires= {'nvim-lua/plenary.nvim'}
+  }
+
   -- Auto completion
-  use {'hrsh7th/nvim-compe'}
+  use {
+    "hrsh7th/nvim-cmp",
+    requires = {
+      "onsails/lspkind-nvim",
+      "hrsh7th/vim-vsnip",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lua",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+      -- "minhduc0711/cmp-eclim",
+    }
+  }
+
   -- Fuzzy finder
-  -- use {'junegunn/fzf', run = fn['fzf#install']}
-  -- use {'junegunn/fzf.vim'}
-  -- use {'ojroques/nvim-lspfuzzy'} -- Display results from LSP client using fzf
-  use {'nvim-lua/popup.nvim'}
-  use {'nvim-lua/plenary.nvim'}
+  use {'junegunn/fzf'}
+  use {'junegunn/fzf.vim'}
+
   use {
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+
   -- Code commenting
   use {'tpope/vim-commentary'}
   -- Remove highlighting right when searching is done
@@ -167,7 +197,10 @@ packer.startup(function(use)
   -- More details: https://github.com/neovim/neovim/issues/12579
   use {'folke/lsp-colors.nvim'}
   -- Pretty statusline
-  use {'hoob3rt/lualine.nvim'}
+  use {'nvim-lualine/lualine.nvim'}
+
+  -- Display colors in CSS
+  use {'ap/vim-css-color'}
 end)
 
 ---------- MORE SPECIFIC CONFIGURATIONS ----------
@@ -181,28 +214,52 @@ g.pyindent_disable_parentheses_indenting = 1
 -- GAML syntax highlighting
 cmd 'au BufRead,BufNewFile *.gaml setlocal filetype=gaml'
 
--- -- nvim-autopairs
--- local npairs=require('nvim-autopairs')
--- npairs.setup()
--- -- skip it, if you use another global object
--- _G.MUtils= {}
--- vim.g.completion_confirm_key = ""
--- MUtils.completion_confirm=function()
---   return npairs.autopairs_cr()
--- end
--- map('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true})
--- npairs.clear_rules()
--- local endwise = require('nvim-autopairs.ts-rule').endwise
--- npairs.add_rules({
--- -- then$ is a lua regex
--- -- end is a match pair
--- -- lua is a filetype
--- -- if_statement is a treesitter name. set it = nil to skip check with treesitter
---   endwise('{$', '}', 'python', nil)
--- })
+-- LSP settings
+local lspconfig = require 'lspconfig'
 
+-- nvim-cmp supports additional completion capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
--- Built-in LSP diagnostics
+-- Enable the following language servers
+local servers = { 'clangd', 'pyright', 'texlab' }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities
+  }
+end
+-- Customize lua LSP a bit
+lspconfig.sumneko_lua.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
+    }
+  }
+}
+
+-- -- Custom vim commands for jdtls
+-- require'jdtls.setup'.add_commands()
+
+-- Enable metals for Scala files
+cmd [[augroup lsp]]
+cmd [[au!]]
+cmd [[au FileType scala,sbt lua require("metals").initialize_or_attach(Metals_config)]]
+cmd [[augroup end]]
+
+Metals_config = require("metals").bare_config()
+Metals_config.init_options.statusBarProvider = "on"
+Metals_config.capabilities = capabilities
+Metals_config.on_attach = on_attach
+
+-- Enable help messages for nvim-metals
+vim.opt_global.shortmess:remove("F")
+
+-- Show LSP diagnostics
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     underline = true,
@@ -211,11 +268,12 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
     update_in_insert = false,
   }
 )
+
 -- Custom gutter signs
-cmd 'sign define LspDiagnosticsSignError text=x texthl=CocErrorSign numhl=CocErrorSign'
-cmd 'sign define LspDiagnosticsSignWarning text=! texthl=CocWarningSign numhl=CocWarningSign'
-cmd 'sign define LspDiagnosticsSignInformation text=I texthl=CocInfoSign numhl=CocInfoSign'
-cmd 'sign define LspDiagnosticsSignHint text=H texthl=CocHintSign numhl=CocHintSign'
+cmd 'sign define DiagnosticSignError text=x texthl=CocErrorSign numhl=CocErrorSign'
+cmd 'sign define DiagnosticSignWarn text=! texthl=CocWarningSign numhl=CocWarningSign'
+cmd 'sign define DiagnosticSignInfo text=I texthl=CocInfoSign numhl=CocInfoSign'
+cmd 'sign define DiagnosticSignHint text=H texthl=CocHintSign numhl=CocHintSign'
 
 -- Treesitter
 local ts = require 'nvim-treesitter.configs'
@@ -226,65 +284,64 @@ ts.setup {
   indent = { enable = false }  -- not working properly for Python
 }
 
--- Autocompletion with nvim-compe
+-- Autocompletion with nvim-cmp
 opt.completeopt = "menuone,noselect"
-require'compe'.setup {
-  enabled = true;
-  autocomplete = true;
-  debug = false;
-  min_length = 1;
-  preselect = 'disable';
-  throttle_time = 80;
-  source_timeout = 200;
-  incomplete_delay = 400;
-  max_abbr_width = 100;
-  max_kind_width = 100;
-  max_menu_width = 100;
-  documentation = true;
-  source = {
-    path = true;
-    buffer = false;  -- slow on big files
-    calc = false;
-    nvim_lsp = true;
-    nvim_lua = true;
-    vsnip = false;
-    ultisnips = false;
-    omni = false;
-  };
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+end
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+local cmp = require'cmp'
+cmp.setup {
+  preselect = cmp.PreselectMode.None,
+  sources = {
+    { name = 'nvim_lsp'},
+    { name = 'nvim_lua'},
+    { name = 'path'},
+    { name = 'eclim', keyword_length = 3 },
+    -- { name = 'buffer'}
+  },
+  mapping = {
+    ['<C-t>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true
+    },
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif vim.fn["vsnip#available"]() == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
+      end
+    end, { "i", "s" }),
+  },
+  formatting = {
+    format = require("lspkind").cmp_format({with_text = true, menu = ({
+      buffer = "[Buffer]",
+      nvim_lsp = "[LSP]",
+      luasnip = "[LuaSnip]",
+      nvim_lua = "[Lua]",
+      eclim = "[Eclim]",
+    })}),
+  },
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
 }
--- Navigate auto completion menu with <TAB>
-cmd [[inoremap <silent><expr> <TAB> pumvisible() ? "\<C-n>" : "\<TAB>"]]
-cmd [[inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"]]
--- Confirm completion
-map('i', '<C-t>', 'compe#confirm("<C-t>")', {expr = true})
-
--- LSPinstall
-local function setup_servers()
-  require'lspinstall'.setup()
-  local servers = require'lspinstall'.installed_servers()
-  for _, server in pairs(servers) do
-    local config = {}
-    if server == "lua" then
-      config.settings = {
-        Lua = {
-          diagnostics = {
-            globals = {'vim'}
-          }
-        }
-      }
-    end
-    require'lspconfig'[server].setup(config)
-  end
-end
-setup_servers()
--- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
-require'lspinstall'.post_install_hook = function ()
-  setup_servers() -- reload installed servers
-  cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
-end
-
--- Custom vim commands for jdtls
-require'jdtls.setup'.add_commands()
 
 -- Telescope
 require('telescope').setup{
@@ -293,13 +350,28 @@ require('telescope').setup{
     layout_config = {
       width = 0.9,
       height = 0.9
-    }
+    },
+    mappings = {
+      i = {
+        ["<esc>"] = require('telescope.actions').close
+      },
+    },
+    extensions = {
+      fzf = {
+        fuzzy = true,                    -- false will only do exact matching
+        override_generic_sorter = false, -- override the generic sorter
+        override_file_sorter = true,     -- override the file sorter
+        case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                         -- the default case_mode is "smart_case"
+      }
+    },
     -- path_display = { "shorten" },
+    dynamic_preview_title = false
   }
 }
-map('n', '<C-p>', ':Telescope find_files<CR>', {silent = true})
-map('n', '<C-f>', ':Telescope live_grep<CR>' , {silent = true})
-map('n', '<C-e>', ':Telescope lsp_document_diagnostics<CR>' , {silent = true})
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+require('telescope').load_extension('fzf')
 -- Use Telescope for Java code actions
 local finders = require'telescope.finders'
 local sorters = require'telescope.sorters'
@@ -333,19 +405,17 @@ require('jdtls.ui').pick_one_async = function(items, prompt, label_fn, cb)
   }):find()
 end
 
--- -- fuzzy search with fzf
--- map('n', '<C-p>', ':Files<CR>', {silent = true})
--- map('n', '<C-f>', ':Rg<CR>' , {silent = true})
--- -- Make fzf (Rg) ignore file names when searching in files' content
--- cmd [[command! -bang -nargs=* Rg call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case ".shellescape(<q-args>), 1, {'options': '--delimiter : --nth 4..'}, <bang>0)]]
--- -- Integrate fzf with LSP
--- local lspfuzzy = require 'lspfuzzy'
--- lspfuzzy.setup {}
+-- fzf configs
+g.fzf_layout = { window = { width= 0.9, height= 0.9 } }
+-- Show tails of file paths
+cmd [[command! -bang -nargs=? -complete=dir Files call fzf#vim#files(<q-args>, fzf#vim#with_preview({'options': ['--keep-right']}), <bang>0)]]
+-- Make fzf (Rg) ignore file names when searching in files' content
+cmd [[command! -bang -nargs=* Rg call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case ".shellescape(<q-args>), 1, fzf#vim#with_preview({'options': '--delimiter : --nth 4..'}), <bang>0)]]
 
 -- Indent defaults (does not override vim-sleuth's indent detection)
 if g._has_set_default_indent_settings == nil then
   -- Set the indent level to 2 spaces for the following file types.
-  cmd 'autocmd FileType lua,typescript,javascript,jsx,tsx,css,html,ruby,elixir,kotlin,vim,plantuml setlocal expandtab tabstop=2 shiftwidth=2'
+  cmd 'autocmd FileType lua,typescript,javascript,jsx,tsx,css,html,ruby,elixir,kotlin,vim,plantuml,scheme setlocal expandtab tabstop=2 shiftwidth=2'
   -- Defaults to 4 spaces for the rest
   opt.expandtab = true
   opt.tabstop = 4
@@ -371,15 +441,37 @@ require('gitsigns').setup {
     changedelete = {hl = 'GitGutterChangeDelete', text = '~_', numhl='GitGutterChangeDelete'},
   },
   numhl = true,
-  status_formatter=format_status
-}
+  status_formatter=format_status,
+  on_attach = function(bufnr)
+    local function gs_map(mode, lhs, rhs, opts)
+        opts = vim.tbl_extend('force', {noremap = true, silent = true}, opts or {})
+        vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+    end
 
--- tmux + vim navigation
-g.tmux_navigator_no_mappings = 1
-map('n', '<M-h>', ':TmuxNavigateLeft<cr>', {silent = true})
-map('n', '<M-n>', ':TmuxNavigateDown<cr>', {silent = true})
-map('n', '<M-e>', ':TmuxNavigateUp<cr>', {silent = true})
-map('n', '<M-i>', ':TmuxNavigateRight<cr>', {silent = true})
+    -- Navigation
+    gs_map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", {expr=true})
+    gs_map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", {expr=true})
+
+    -- Actions
+    gs_map('n', '<leader>hs', ':Gitsigns stage_hunk<CR>')
+    gs_map('v', '<leader>hs', ':Gitsigns stage_hunk<CR>')
+    gs_map('n', '<leader>hr', ':Gitsigns reset_hunk<CR>')
+    gs_map('v', '<leader>hr', ':Gitsigns reset_hunk<CR>')
+    gs_map('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
+    gs_map('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
+    gs_map('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
+    gs_map('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
+    gs_map('n', '<leader>hb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
+    gs_map('n', '<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
+    gs_map('n', '<leader>hd', '<cmd>Gitsigns diffthis<CR>')
+    gs_map('n', '<leader>hD', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
+    gs_map('n', '<leader>td', '<cmd>Gitsigns toggle_deleted<CR>')
+
+    -- Text object
+    gs_map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    gs_map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  end
+}
 
 -- Live REPL with vim-slime
 g.slime_target = "tmux"
@@ -396,6 +488,7 @@ g.qfenter_keymap = {
   hopen = {'<C-x>'},
   topen = {'<C-t>'}
 }
+g.qfenter_enable_autoquickfix = 0
 
 -- Colorscheme
 opt.termguicolors = true
@@ -415,9 +508,10 @@ local lualine_sections = {
   lualine_c = {
     'filename',
     {'diagnostics',
-      sources = {'nvim_lsp'},
+      sources = {'nvim_diagnostic'},
       symbols = {error = 'E:', warn = 'W:', info = 'I:', hint = 'H:'}
-    }
+    },
+    'g:metals_status'
   },
   lualine_x = {'filetype'},
   lualine_y = {{'fileformat', icons_enabled = false}, 'encoding'},
