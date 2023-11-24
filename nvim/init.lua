00369db94921ed5edbf8ce1a986374b78eb6a396
minhduc0711 <phamminhduc0711@gmail.com>
2023-11-24 10:56:59 +0100

    Fix fidget.nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -61,7 +61,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = { window = { blend = 0 } } },
+      { 'j-hui/fidget.nvim', opts = { notification = { window = { winblend = 0 } } } },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
