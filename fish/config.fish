019f999e6cb0eca02bad2cccb2b99776150ad0d6
minhduc0711 <phamminhduc0711@gmail.com>
2024-05-30 00:51:08 +0200

    Add config for fish shell

---
+++ fish/config.fish
@@ -0,0 +1,32 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+fish_vi_key_bindings
+fish_config theme choose Tomorrow
+
+eval (dircolors -c ~/.dircolors)
+
+# FZF
+fzf_configure_bindings --directory=\cp
+set fzf_preview_file_cmd cat -n
+
+# Environment variables
+set -x YDIFF_OPTIONS '-s -w0 --wrap'
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/minhduc0711/miniforge3/bin/conda
+    eval /home/minhduc0711/miniforge3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/minhduc0711/miniforge3/etc/fish/conf.d/conda.fish"
+        . "/home/minhduc0711/miniforge3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/minhduc0711/miniforge3/bin" $PATH
+    end
+end
+
+if test -f "/home/minhduc0711/miniforge3/etc/fish/conf.d/mamba.fish"
+    source "/home/minhduc0711/miniforge3/etc/fish/conf.d/mamba.fish"
+end
+# <<< conda initialize <<<
